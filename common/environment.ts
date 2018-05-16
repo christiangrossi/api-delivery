a8a87670360a8cf53bb90ba16f63e9d60dc57a8e
Jaison Pereira <jaison@logusretail.com.br>
2018-05-16 09:33:12 -0300

    teste with port

--- common/environment.ts
+++ common/environment.ts
@@ -2,7 +2,7 @@
 //localhost teste
 //db: { url: process.env.DB_URL || 'mongodb://localhost/api-delivery' },
 export const environment = {
-    server: { port: process.env.SERVER_PORT || 3000 },
+    server: { port: process.env.PORT || 3000 },
     db: { url: process.env.DB_URL || 'mongodb://root:root@ds259325.mlab.com:59325/api-delivery' },
     security: { saltRounds: process.env.SALT || 10 }
 }
--- target/common/environment.js
+++ target/common/environment.js
@@ -3,7 +3,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 //localhost teste
 //db: { url: process.env.DB_URL || 'mongodb://localhost/api-delivery' },
 exports.environment = {
-    server: { port: process.env.SERVER_PORT || 3000 },
+    server: { port: process.env.PORT || 3000 },
     db: { url: process.env.DB_URL || 'mongodb://root:root@ds259325.mlab.com:59325/api-delivery' },
     security: { saltRounds: process.env.SALT || 10 }
 };
